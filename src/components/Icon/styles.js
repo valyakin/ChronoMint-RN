4a07427c3db0d538144528e867d325c05ac54286
Oleg Dizus <olegdizus@gmail.com>
2017-11-06 15:37:08 +0300

    Add Icon component

---
+++ src/components/Icon/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+import { UNIT } from '@styleConstants'
+
+export default StyleSheet.create({
+  container: {
+    width: 3 * UNIT,
+    height: 3 * UNIT,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Text } from './Text'
 export { Logo } from './Logo'
 export { FetchingIndicator } from './FetchingIndicator'
+export { Icon } from './Icon'
