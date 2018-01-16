f963ed808ca9d58ab3926becffd8fe5a3b4bc72c
Oleg Dizus <olegdizus@gmail.com>
2018-01-16 19:04:01 +0300

    Fix react-native-navigation and rm react-native-os

--- android/app/src/main/java/io/chronobank/chronomintapp/MainApplication.java
+++ android/app/src/main/java/io/chronobank/chronomintapp/MainApplication.java
@@ -1,59 +1,21 @@
 package io.chronobank.chronomintapp;
 
-import com.reactnativenavigation.NavigationApplication;
+import android.support.annotation.Nullable;
 
+import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactApplication;
 import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
-import com.peel.react.rnos.RNOSModule;
 import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
-import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
-import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
-import com.peel.react.rnos.RNOSModule;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeFingerprintScannerPackage(),
-            new RNOSModule(),
-            new ReactNativeDocumentPicker(),
-            new ReactNativeDocumentPicker(),
-            new ReactNativeDocumentPicker(),
-            new RNOSModule(),
-            new NavigationReactPackage(),
-            new RNDeviceInfo(),
-            new ReactNativeLocalizationPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
 
   @Override
   public boolean isDebug() {
@@ -61,9 +23,24 @@ public class MainApplication extends NavigationApplication {
       return BuildConfig.DEBUG;
   }
 
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+          new ReactNativeFingerprintScannerPackage(),
+          new ReactNativeDocumentPicker(),
+          new RNDeviceInfo(),
+          new ReactNativeLocalizationPackage()
+    );
+  }
+
+  @Nullable
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+      return getPackages();
+  }
+
+  @Nullable
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
   }
 }
