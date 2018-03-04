77b7a2ffd10846b178dad21ba0bfa466d57cd1fe
Oleg Dizus <olegdizus@gmail.com>
2018-03-04 10:53:08 +0300

    Use ScrollView on Wallet screen

--- src/screens/Wallet/Wallet.js
+++ src/screens/Wallet/Wallet.js
@@ -1,47 +1,53 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View, StyleSheet } from 'react-native'
+import { ScrollView,  View, StyleSheet } from 'react-native'
 import Text from '../../components/Text'
 import { COLOR_BACKGROUND } from '../../constants/styles'
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLOR_BACKGROUND,
-  },
-})
-
 const Wallet = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Wallet:</Text>
-      {props.addresses.map((address) => {
-        const walletId = address.get('id')
-        const walletAddress = address.get('address')
-
-        return walletAddress ? (
-          <Text key={walletId}>
-            {walletId}: {walletAddress}
-          </Text>
-        ) : null
-      })}
-      <Text>Balances:</Text>
-      {props.balances.map((balance) => {
-        const balanceId = balance.get('id')
-        const balanceAmount = balance.get('amout')
-
-        return (
-          <Text key={balanceId}>
-            {balanceId}: {balanceAmount || 0}
-          </Text>
-        )
-      })}
-    </View>
+    <ScrollView style={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text>Wallet:</Text>
+        {props.addresses.map((address) => {
+          const walletId = address.get('id')
+          const walletAddress = address.get('address')
+          
+          return walletAddress ? (
+            <Text key={walletId}>
+              {walletId}: {walletAddress}
+            </Text>
+          ) : null
+        })}
+        <Text>Balances:</Text>
+        {props.balances.map((balance) => {
+          const balanceId = balance.get('id')
+          const balanceAmount = balance.get('amout')
+          
+          return (
+            <Text key={balanceId}>
+              {balanceId}: {balanceAmount || 0}
+            </Text>
+          )
+        })}
+      </View>
+    </ScrollView>
   )
 }
 
 const mapStateToProps = (state) => ({
   balances: state.get('mainWallet').get('balances').get('list').toArray(),
   addresses: state.get('mainWallet').get('addresses').get('list').toArray(),
+})
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    backgroundColor: COLOR_BACKGROUND,
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
 })
 
 export default connect(
