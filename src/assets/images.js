3969aed85616f86910161eabf902269cabd01576
Oleg Dizus <olegdizus@gmail.com>
2018-03-17 18:13:21 +0300

    Refactor images list

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,53 +1,31 @@
-import alert from './images/alert.png'
-import burger from './images/burger.png'
-import check from './images/check.png'
-import checkboxChecked from './images/checkbox-checked.png'
-import checkbox from './images/checkbox.png'
-import chevronLeft from './images/chevron-left.png'
-import chevronRight from './images/chevron-right.png'
-import paperclip from './images/paperclip.png'
-import comment from './images/comment.png'
-import fingerprint from './images/fingerprint.png'
-import gradient from './images/gradient.png'
-import helpCircle from './images/help-circle.png'
-import key from './images/key.png'
-import logo from './images/logo.png'
-import mnemonic from './images/mnemonic.png'
-import network from './images/network.png'
-import splash from './images/splash.png'
-import transactionReceiving from './images/transaction-receiving.png'
-import tune from './images/tune.png'
-import uport from './images/uport.png'
-import walletBadgeDefault from './images/wallet-badge-default.png'
-import walletBadgeLocked from './images/wallet-badge-locked.png'
-import walletBadgeShared from './images/wallet-badge-shared.png'
-import walletBitcoin from './images/wallet-bitcoin.png'
-import wallet from './images/wallet.png'
-
 export default {
-  alert,
-  burger,
-  check,
-  checkboxChecked,
-  checkbox,
-  chevronLeft,
-  chevronRight,
-  paperclip,
-  comment,
-  fingerprint,
-  gradient,
-  helpCircle,
-  key,
-  logo,
-  mnemonic,
-  network,
-  splash,
-  transactionReceiving,
-  tune,
-  uport,
-  walletBadgeDefault,
-  walletBadgeLocked,
-  walletBadgeShared,
-  walletBitcoin,
-  wallet,
+  alert: require('./images/alert.png'),
+  burger: require('./images/burger.png'),
+  check: require('./images/check.png'),
+  checkboxChecked: require('./images/checkbox-checked.png'),
+  checkbox: require('./images/checkbox.png'),
+  chevronLeft: require('./images/chevron-left.png'),
+  chevronRight: require('./images/chevron-right.png'),
+  paperclip: require('./images/paperclip.png'),
+  comment: require('./images/comment.png'),
+  fingerprint: require('./images/fingerprint.png'),
+  gradient: require('./images/gradient.png'),
+  helpCircle: require('./images/help-circle.png'),
+  key: require('./images/key.png'),
+  logo: require('./images/logo.png'),
+  mnemonic: require('./images/mnemonic.png'),
+  network: require('./images/network.png'),
+  splash: require('./images/splash.png'),
+  transactionReceiving: require('./images/transaction-receiving.png'),
+  tune: require('./images/tune.png'),
+  uport: require('./images/uport.png'),
+  plus: require('./images/plus.png'),
+  walletBadgeDefault: require('./images/wallet-badge-default.png'),
+  walletBadgeLocked: require('./images/wallet-badge-locked.png'),
+  walletBadgeShared: require('./images/wallet-badge-shared.png'),
+  walletBitcoin: require('./images/wallet-bitcoin.png'),
+  walletLitecoin: require('./images/wallet-litecoin.png'),
+  walletEtherium: require('./images/wallet-etherium.png'),
+  walletNem: require('./images/wallet-nem.png'),
+  wallet: require('./images/wallet.png'),
 }
