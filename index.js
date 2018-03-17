082d9e713d01529d907eccd79e91a03aa644d411
Oleg Dizus <olegdizus@gmail.com>
2018-03-17 17:28:51 +0300

    Ignore some warnings

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 console.ignoredYellowBox = [
   'Warning: componentWillMount',
   'Warning: componentWillReceiveProps',
+  'Warning: componentWillUpdate',
+  'Can\'t restore local session',
 ]
 
 require('./src/app.js')
