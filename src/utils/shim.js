b44620fa70d1b1c1f9bd9b7e56c01a54f26f1ba2
Alexey Ozerov <alexozerov@gmail.com>
2018-03-28 11:50:12 +0300

    Added polyfill for the Error. captureStackTrace

--- src/utils/shim.js
+++ src/utils/shim.js
@@ -23,6 +23,11 @@ if (typeof process === 'undefined') {
 process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
+// Polyfill for the Error.captureStackTrace (this is NodeJS core's method)
+if (typeof Error.captureStackTrace === 'undefined') {
+  global.Error.captureStackTrace = require('capture-stack-trace')
+}
+
 // global.location = global.location || { port: 80 }
 const isDev = typeof __DEV__ === 'boolean' && __DEV__
 process.env['NODE_ENV'] = isDev ? 'development' : 'production'
