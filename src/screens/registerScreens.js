89a846579c01d21573227be046480c04b26fda26
Oleg Dizus <olegdizus@gmail.com>
2018-04-12 17:24:38 +0300

    Remove old screens
    Add screens

--- src/screens/registerScreens.js
+++ src/screens/registerScreens.js
@@ -15,21 +15,10 @@ import AddStandardWallet from './AddStandardWallet'
 import AddTimeLockedWallet from './AddTimeLockedWallet'
 import AddTokenToAdvancedWallet from './AddTokenToAdvancedWallet'
 import AddWallet from './AddWallet'
-import ChangeLanguage from './ChangeLanguage'
 import ConfirmSend from './ConfirmSend'
 import CreateWallet from './CreateWallet'
 import Download2FAApp from './Download2FAApp'
 import Drawer from './Drawer'
-import EnterMnemonic from './EnterMnemonic'
-import EnterPinCode from './EnterPinCode'
-import EnterPrivateKey from './EnterPrivateKey'
-import EnterWalletPassword from './EnterWalletPassword'
-import GenerateMnemonic from './GenerateMnemonic'
-import GenerateWalletFile from './GenerateWalletFile'
-import LoginSettings from './LoginSettings'
-import PickWalletFile from './PickWalletFile'
-import SelectLoginOption from './SelectLoginOption'
-import SelectNetwork from './SelectNetwork'
 import SelectToken from './SelectToken'
 import Send from './Send'
 import Wallet from './Wallet'
@@ -48,21 +37,10 @@ export default function registerScreens (store, Provider) {
   Navigation.registerComponent('AddTimeLockedWallet', () => AddTimeLockedWallet, store, Provider)
   Navigation.registerComponent('AddTokenToAdvancedWallet', () => AddTokenToAdvancedWallet, store, Provider)
   Navigation.registerComponent('AddWallet', () => AddWallet, store, Provider)
-  Navigation.registerComponent('ChangeLanguage', () => ChangeLanguage, store, Provider)
   Navigation.registerComponent('ConfirmSend', () => ConfirmSend, store, Provider)
   Navigation.registerComponent('CreateWallet', () => screenLayout(LoginScreenLayout)(CreateWallet), store, Provider)
   Navigation.registerComponent('Download2FAApp', () => Download2FAApp, store, Provider)
   Navigation.registerComponent('Drawer', () => Drawer, store, Provider)
-  Navigation.registerComponent('EnterMnemonic', () => EnterMnemonic, store, Provider)
-  Navigation.registerComponent('EnterPinCode', () => EnterPinCode, store, Provider)
-  Navigation.registerComponent('EnterPrivateKey', () => EnterPrivateKey, store, Provider)
-  Navigation.registerComponent('EnterWalletPassword', () => EnterWalletPassword, store, Provider)
-  Navigation.registerComponent('GenerateMnemonic', () => GenerateMnemonic, store, Provider)
-  Navigation.registerComponent('GenerateWalletFile', () => GenerateWalletFile, store, Provider)
-  Navigation.registerComponent('LoginSettings', () => LoginSettings, store, Provider)
-  Navigation.registerComponent('PickWalletFile', () => PickWalletFile, store, Provider)
-  Navigation.registerComponent('SelectLoginOption', () => SelectLoginOption, store, Provider)
-  Navigation.registerComponent('SelectNetwork', () => SelectNetwork, store, Provider)
   Navigation.registerComponent('SelectToken', () => SelectToken, store, Provider)
   Navigation.registerComponent('Send', () => Send, store, Provider)
   Navigation.registerComponent('Wallet', () => Wallet, store, Provider)
