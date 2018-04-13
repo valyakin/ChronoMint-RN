491703a2db790e4ea3bce538333bfe5643e76ab4
Oleg Dizus <olegdizus@gmail.com>
2018-04-13 14:17:38 +0300

    Add missing licenses

--- src/screens/SelectToken.js
+++ src/screens/SelectToken.js
@@ -1,4 +1,10 @@
-/* @flow */
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ *
+ * @flow
+ */
+
 import React, { PureComponent } from 'react'
 import {
   ListView,
