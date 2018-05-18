acf1a21407ca1f5dd05c3832dfb655e9a8c8f3e3
Oleg Dizus <olegdizus@gmail.com>
2018-05-18 19:22:41 +0300

    Add SelectLanguage screen and container

--- src/registerScreens.js
+++ src/registerScreens.js
@@ -27,6 +27,7 @@ import EnterPin from './containers/EnterPinContainer'
 import EnterPrivateKey from './containers/EnterPrivateKeyContainer'
 import GenerateMnemonic from './containers/GenerateMnemonicContainer'
 import SelectAccount from './containers/SelectAccountContainer'
+import SelectLanguage from './containers/SelectLanguageContainer'
 import SelectToken from './screens/SelectToken'
 import Send from './containers/SendContainer'
 import Wallet from './containers/WalletContainer'
@@ -58,6 +59,7 @@ export default function registerScreens (store, Provider) {
   Navigation.registerComponent('EnterPrivateKey', () => screenLayout(LoginScreenLayout)(EnterPrivateKey), store, Provider)
   Navigation.registerComponent('GenerateMnemonic', () => screenLayout(LoginScreenLayout)(GenerateMnemonic), store, Provider)
   Navigation.registerComponent('SelectAccount', () => screenLayout(LoginScreenLayout)(SelectAccount), store, Provider)
+  Navigation.registerComponent('SelectLanguage', () => SelectLanguage, store, Provider)
   Navigation.registerComponent('SelectToken', () => SelectToken, store, Provider)
   Navigation.registerComponent('Send', () => Send, store, Provider)
   Navigation.registerComponent('Wallet', () => Wallet, store, Provider)
