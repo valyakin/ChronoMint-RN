3e5ef0ce310c205ec73f4bdd84e9ebdf46fd5c0a
Oleg Dizus <olegdizus@gmail.com>
2018-05-18 19:24:10 +0300

    Remake navigator roots

--- src/app.js
+++ src/app.js
@@ -14,24 +14,82 @@ import registerScreens from './registerScreens'
 
 registerScreens(store, Provider)
 
-Navigation.startSingleScreenApp({
-  screen: {
-    screen: 'CreateWallet',
-  },
-  appStyle: {
-    disabledBackGesture: true,
-    hideBackButtonTitle: true,
-    navBarBackgroundColor: '#614DBA',
-    navBarButtonColor: '#FFFFFF',
-    navBarTextColor: '#BDB2FF',
-    screenBackgroundColor: '#242045',
-    statusBarTextColorScheme: 'light',
-  },
-  drawer: {
-    left: {
-      screen: 'Drawer',
-    },
-    animationType: 'parallax',
-    disableOpenGesture: true,
-  },
-})
+let currentRoot = ''
+
+export default function startAppRoot (root: string) {
+  if (currentRoot === root) return 
+
+  switch (root) {
+    case 'wallet': {
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: 'WalletsList',
+        },
+        appStyle: {
+          disabledBackGesture: true,
+          hideBackButtonTitle: true,
+          navBarBackgroundColor: '#614DBA',
+          navBarButtonColor: '#FFFFFF',
+          navBarTextColor: '#BDB2FF',
+          screenBackgroundColor: '#242045',
+          statusBarTextColorScheme: 'light',
+        },
+        drawer: {
+          left: {
+            screen: 'Drawer',
+          },
+          animationType: 'parallax',
+          disableOpenGesture: true,
+        },
+      })
+
+      break
+    }
+    case 'login':
+    default: {
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: 'CreateWallet',
+          navigatorStyle: {
+            navBarHidden: true,
+          },
+        },
+        appStyle: {
+          disabledBackGesture: true,
+          drawUnderNavBar: true,
+          hideBackButtonTitle: true,
+          navBarButtonColor: '#FFFFFF',
+          navBarNoBorder: true,
+          navBarTextColor: '#BDB2FF',
+          navBarTextFontSize: 16,
+          navBarTranslucent: true,
+          navBarTransparent: true,
+          screenBackgroundColor: '#242045',
+          statusBarTextColorScheme: 'light',
+          topBarElevationShadowEnabled: false,
+        },
+        drawer: {
+          left: {
+            screen: 'SelectNetwork',
+          },
+          right: {
+            screen: 'SelectLanguage',
+          },
+          animationType: 'parallax',
+          disableOpenGesture: true,
+          style: {
+            drawerShadow: false,
+            leftDrawerWidth: 75,
+            rightDrawerWidth: 75,
+          },
+        },
+      })
+
+      break
+    }
+  }
+
+  currentRoot = root
+}
+
+startAppRoot('login')
