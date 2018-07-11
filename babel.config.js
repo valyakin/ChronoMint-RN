46a4cfeb71072eed13ba35631006837b36f846c9
Alexey Ozerov <alexozerov@gmail.com>
2018-07-11 15:03:16 +0300

    Remove unnecessary dependencies from XCode project
    Removed libRNFS.a after removing of NPM package react-native-fs from dependencies
    Also formatted file babel.config.js
    Applied comments
    Fix

--- babel.config.js
+++ babel.config.js
@@ -1,50 +1,57 @@
 module.exports = {
   presets: [
-    '@babel/env',
-    '@babel/preset-react',
-    '@babel/flow'
+    "@babel/env",
+    "@babel/preset-react",
+    "@babel/flow"
   ],
   env: {
     test: {
       presets: [
-        ['@babel/env', { modules: false }],
-        '@babel/preset-react', '@babel/flow'
+        [
+          "@babel/env",
+          { modules: false }
+        ],
+        "@babel/preset-react",
+        "@babel/flow"
       ],
       plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-transform-modules-commonjs'
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-transform-modules-commonjs"
       ],
     },
   },
-  babelrcRoots: 'packages/*',
+  babelrcRoots: "packages/**",
   sourceMaps: true,
   plugins: [
     [
-      '@babel/plugin-transform-runtime',
+      "@babel/plugin-transform-runtime",
       {
         helpers: false,
         polyfill: false,
         regenerator: true,
-        moduleName: '@babel/runtime',
+        moduleName: "@babel/runtime",
       },
     ],
-    '@babel/plugin-transform-flow-strip-types',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-class-properties',
-    'functional-hmr',
-    'transform-new-target',
-    'transform-exponentiation-operator',
+    "@babel/plugin-transform-flow-strip-types",
     [
-      'babel-plugin-transform-builtin-extend',
+      "@babel/plugin-proposal-decorators",
+      { legacy: true }
+    ],
+    "@babel/plugin-proposal-class-properties",
+    "functional-hmr",
+    "transform-new-target",
+    "transform-exponentiation-operator",
+    [
+      "babel-plugin-transform-builtin-extend",
       {
-        globals: ['Error', 'Array'],
-      },
+        globals: ["Error", "Array"]
+      }
     ],
     [
-      'transform-inline-environment-variables',
+      "transform-inline-environment-variables",
       {
-        include: ['NODE_ENV', 'REDUX_LOGGER'],
-      },
+        include: ["NODE_ENV", "REDUX_LOGGER"]
+      }
     ],
   ],
 }
